f25ebf6c6ca2dc64305ad1eced2f11ce3b7db95b
anndyyz <anda2546r@gmail.com>
2024-03-30 19:58:52 +0700

    ex6

--- 65030276_Lab06/Lab06_Ex01/obj/Debug/net8.0/Lab06_Ex01.AssemblyInfo.cs
+++ 65030276_Lab06/Lab06_Ex01/obj/Debug/net8.0/Lab06_Ex01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab06_Ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba58a10f7b9fffc753084682731c49ff3e1b6969")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+009f7e0d6319eb8556db6704b50e432dbb7d988a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab06_Ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab06_Ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
